478c342a40b895f386babfac484011e3c34e5a9e
Deepak Pardeshi <deepak.pardeshi@evry.com>
2019-08-29 15:58:33 +0530

    counter example using useReducer hook

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,56 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+// import { Text, View, Button, StyleSheet } from 'react-native'
+
+// const CounterScreen = () => {
+
+//     const [counter, setCounter] = useState(0)
+
+//     return (
+//         <View>
+//             <Button
+//                 title='Increment'
+//                 onPress={() => { setCounter(counter + 1) }}
+//             ></Button>
+//             <Button
+//                 title='Decrement'
+//                 onPress={() => { setCounter(counter - 1) }}
+//             ></Button>
+//             <Text>Counter Count {counter}</Text>
+//         </View>
+//     );
+// }
+
+// export default CounterScreen;
+
+// using useReducer hook
+import React, { useReducer } from 'react'
 import { Text, View, Button, StyleSheet } from 'react-native'
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { ...state, counter: state.counter + action.payload }
+        case 'decrement':
+            return { ...state, counter: state.counter - action.payload }
+        default :
+            return state;
+    }
+}
 const CounterScreen = () => {
 
-    const [counter, setCounter] = useState(0)
+    const [state, dispatch] = useReducer(reducer, {counter:0} )
 
     return (
         <View>
             <Button
                 title='Increment'
-                onPress={() => { setCounter(counter + 1) }}
+                onPress={() => { dispatch({ type:'increment', payload: 1}) }}
             ></Button>
             <Button
                 title='Decrement'
-                onPress={() => { setCounter(counter - 1) }}
+                onPress={() => { dispatch({ type:'decrement', payload: 1}) }}
             ></Button>
-            <Text>Counter Count {counter}</Text>
+            <Text>Counter Count {state.counter}</Text>
         </View>
     );
 }
